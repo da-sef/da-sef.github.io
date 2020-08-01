a9363c263ac245e1aa0a336fbb64b84fe8c5e07a
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-08-01 16:07:40 +0530

    Add copyright

--- components/footer.vue
+++ components/footer.vue
@@ -1,20 +1,28 @@
 <template>
   <footer class="footer bg-white p-2">
-    <div class="text-sm flex">
+    <div class="text-sm flex flex-col sm:flex-row text-center">
       <router-link to="/faqs" class="m-2">
         FAQs
       </router-link>
       <router-link to="/request" class="m-2">
         Request Help
       </router-link>
-      <a href="http://sbg.daiict.ac.in" class="m-2" target="_blank">
-        SBG
+      <a href="mailto:sef@daiict.ac.in" class="m-2" target="_blank">
+        Contact Us
       </a>
       <div class="skip"></div>
+    </div>
+    <div class="text-center sm:text-left text-xs mt-5">
       <span class="m-2">
         Created by
         <a href="https://github.com/sanket143" target="_blank">
           Sanket Chaudhari
+        </a>
+      </span>
+      <span class="m-2">
+        © 2020
+        <a href="http://sbg.daiict.ac.in" target="_blank">
+          SBG, DA-IICT
         </a>
       </span>
     </div>
